044289adc338b40b52cf24e5caaaa77d13732518
camiviih <camilivit.moreira@gmail.com>
2025-07-30 15:36:50 -0300

    Initial Commit

---
+++ CuidaDoDino/CuidaDoDinoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CuidaDoDinoApp.swift
+//  CuidaDoDino
+//
+//  Created by Camili Moreira on 30/07/25.
+//
+
+import SwiftUI
+
+@main
+struct CuidaDoDinoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
